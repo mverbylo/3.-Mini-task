eb12342450ae00c18fe45f8c1dbf7620095b7b91
Mariia Verbylo <mariaverbilo@gmail.com>
2022-09-12 21:00:05 +0300

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+/*Минитаск Функции-конструкторы+прототипы
+Создать функцию-конструктор User
+у пользователей должны быть свойства:
+firstName
+lastName
+email
+age
+isLoggedIn
+
+
+Создать прототип для обїектов пользователей
+в прототипе реализовать следующий методы:
+getFullName - метод, который вернет строчку, содержащую имя и фамилию пользователя
+logout - метод, который имитирует логаут пользователя, меняя переменную isLoggedIn
+login - метод, который имитирует логин пользователя, меняя переменную isLoggedIn
+*/
+function User(firstName, lastName, email, age) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.email = email;
+  this.age = age;
+  this.isLoggedIn = true;
+}
+const userProto = {
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  logout: function () {
+    return (this.isLoggedIn = false);
+  },
+  login: function () {
+    return (this.isLoggedIn = true);
+  },
+};
+User.prototype = userProto;
+
+// const user1 = new User('Taras', 'Bulba', 'taras@email.com', 64);
