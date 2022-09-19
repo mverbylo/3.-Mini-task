d6be734a2c1acc3d3f1e41fb0e01daad24fa2f47
Mariia Verbylo <mariaverbilo@gmail.com>
2022-09-19 21:07:05 +0300

    Add 4 task

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,97 +1,18 @@
 'use strict';
-// ---------------------------------3.Cтрелочные функции
-//Создайте функцию, которая принимает неограниченное количество входящих данных,
-//и возвращает количество строк, переданных ей в этом массиве.
-//Решить с использованием стрелочных функций.
-
-const countString = (...input) =>
-  input.filter((elem) => (elem = typeof elem === 'string')).length;
-
-countString(2, 'text1', ...[2, 'text2']); //2
-
-//Создайте функцию продвинутого калькулятора.
-// Он принимает знак, а  затем неограниченное количество чисел,
-// над которыми проведет указанную операцию.
-const sum = (...numbers) =>
-  numbers.reduce((res, currentNumber) => {
-    if (
-      typeof res !== 'number' ||
-      typeof currentNumber !== 'number' ||
-      isNaN(res - currentNumber)
-    ) {
-      return null;
+//---------------------------------4. Микротаски Рекурсия.
+//Создать рекурсивную функцию вычисления факториала числа.Факториал - произведение всех натуральных чисел от 1 до x.
+// 5! = 1 * 2 * 3 * 4 * 5 = 120
+function factorial(number) {
+  if (Number.isInteger(number)) {
+    if (number === 0) {
+      return 1;
     }
-    return res + currentNumber;
-  });
-
-const subtraction = (...numbers) =>
-  numbers.reduce((res, currentNumber) => {
-    if (
-      typeof res !== 'number' ||
-      typeof currentNumber !== 'number' ||
-      isNaN(res - currentNumber)
-    ) {
-      return null;
-    }
-    return res - currentNumber;
-  });
-const multiplication = (...numbers) =>
-  numbers.reduce((res, currentNumber) => {
-    if (
-      typeof res !== 'number' ||
-      typeof currentNumber !== 'number' ||
-      isNaN(res - currentNumber)
-    ) {
-      return null;
-    }
-    return res * currentNumber;
-  });
-const division = (...numbers) =>
-  numbers.reduce((res, currentNumber) => {
-    if (
-      typeof res !== 'number' ||
-      typeof currentNumber !== 'number' ||
-      isNaN(res - currentNumber)
-    ) {
-      return null;
-    }
-    return res / currentNumber;
-  });
-
-const calc = function (operator, ...numbers) {
-  switch (operator) {
-    case '+': {
-      return sum(...numbers);
-    }
-    case '-': {
-      return subtraction(...numbers);
-    }
-    case '*': {
-      return multiplication(...numbers);
-    }
-    case '/': {
-      return division(...numbers);
-    }
-    default:
-      result = null;
+    return number > 0
+      ? number * factorial(number - 1)
+      : 'Enter positive number';
   }
-  return result;
-};
-
-calc('+', 10, 2); //12
-calc('+', 10, 2, true); //null
-calc('-', 10, 2); //8
-calc('*', 10, 2); //20
-calc('/', 10, 2); //5
-
-//Имея массив чисел, найдите в нем самое большое.
-//Не используйте циклы или методы перебора массива.
-const arr = [-5, 40, 1000, 90];
-Math.max(...arr); //1000
-
-
-
-
+  return 'Enter intenger number';
+}
 /*---------------------------------1. Минитаск Функции-конструкторы+прототипы
 Создать функцию-конструктор User
 у пользователей должны быть свойства:
@@ -151,4 +72,94 @@ const str2 =
     .split(' ')
     .reduce((word1, word2) => (word1.length > word2.length ? word1 : word2)).length;
 //'10'
+*/
+/* --------------------------------3.Cтрелочные функции
+//Создайте функцию, которая принимает неограниченное количество входящих данных,
+//и возвращает количество строк, переданных ей в этом массиве.
+//Решить с использованием стрелочных функций.
+
+const countString = (...input) =>
+  input.filter((elem) => (typeof elem === 'string')).length;
+
+countString(2, 'text1', ...[2, 'text2']); //2
+
+//Создайте функцию продвинутого калькулятора.
+// Он принимает знак, а  затем неограниченное количество чисел,
+// над которыми проведет указанную операцию.
+const sum = (...numbers) =>
+  numbers.reduce((res, currentNumber) => {
+    if (
+      typeof res !== 'number' ||
+      typeof currentNumber !== 'number' ||
+      isNaN(res - currentNumber)
+    ) {
+      return null;
+    }
+    return res + currentNumber;
+  });
+
+const subtraction = (...numbers) =>
+  numbers.reduce((res, currentNumber) => {
+    if (
+      typeof res !== 'number' ||
+      typeof currentNumber !== 'number' ||
+      isNaN(res - currentNumber)
+    ) {
+      return null;
+    }
+    return res - currentNumber;
+  });
+const multiplication = (...numbers) =>
+  numbers.reduce((res, currentNumber) => {
+    if (
+      typeof res !== 'number' ||
+      typeof currentNumber !== 'number' ||
+      isNaN(res - currentNumber)
+    ) {
+      return null;
+    }
+    return res * currentNumber;
+  });
+const division = (...numbers) =>
+  numbers.reduce((res, currentNumber) => {
+    if (
+      typeof res !== 'number' ||
+      typeof currentNumber !== 'number' ||
+      isNaN(res - currentNumber)
+    ) {
+      return null;
+    }
+    return res / currentNumber;
+  });
+
+const calc = function (operator, ...numbers) {
+  switch (operator.trim()) {
+    case '+': {
+      return sum(...numbers);
+    }
+    case '-': {
+      return subtraction(...numbers);
+    }
+    case '*': {
+      return multiplication(...numbers);
+    }
+    case '/': {
+      return division(...numbers);
+    }
+    default:
+      result = null;
+  }
+  return result;
+};
+
+calc('+', 10, 2); //12
+calc('+', 10, 2, true); //null
+calc('-', 10, 2); //8
+calc('*', 10, 2); //20
+calc('/', 10, 2); //5
+
+//Имея массив чисел, найдите в нем самое большое.
+//Не используйте циклы или методы перебора массива.
+const arr = [-5, 40, 1000, 90];
+Math.max(...arr); //1000
 */
